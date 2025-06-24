eea6d8070e1c6b57eea90dc815e430d1e217c62f
emory645 <emorypp11@gmail.com>
2025-06-24 13:56:26 -0500

    rock

--- index.js
+++ index.js
@@ -20,9 +20,38 @@ function selctorChoice() {
     document.getElementById("your-choice").src = you + '.png';
 }
 opponent = choices[Math.floor(Math.random() * 3)];
-document.getElementById('opponent-choice').src = opponent + ".png"
+document.getElementById('opponent-choice').src = opponent + '.png';
 
     if( you == opponent) {
         yourScore += 1;
         opponentScore += 1;
     }
+    else {
+        if (you ==  'rock') {
+            if(opponent == 'scissors'){
+                yourScore += 1;
+            }
+            else if (opponent == 'paper') {
+                opponentScore += 1;
+            }
+        }
+        else if (you ==  'scissors') {
+            if(opponent == 'paper'){
+                yourScore += 1;
+            }
+            else if (opponent == 'rock') {
+                opponentScore += 1;
+            }
+        }
+        else if (you ==  'paper') {
+            if(opponent == 'rock'){
+                yourScore += 1;
+            }
+            else if (opponent == 'scissors') {
+                opponentScore += 1;
+            }
+        }
+        document.getElementById("your-score").innerText = yourScore;
+        document.getElementById("opponent-score").innerText = opponentScore;
+    }
+    
