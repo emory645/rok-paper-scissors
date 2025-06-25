90734ae624933a82b98b38758b492de3f9a9f8a5
emory645 <emorypp11@gmail.com>
2025-06-25 13:28:42 -0500

    rock

--- index.js
+++ index.js
@@ -1,57 +1,61 @@
 var you;
 var yourScore = 0;
-var opponentScore = 0;
 var opponent;
+var opponentScore = 0;
 
-var choices = ['rock','paper','scissors'];
+var choices = ["rock", "paper", "scissors"];
 
 window.onload = function() {
-    for(let i = 0; i < 3; i++) {
-        let choice = document.createElement('img')
+    for (let i = 0; i < 3; i++) {
+        // <img id="rock" src="rock.png">
+        let choice = document.createElement("img");
         choice.id = choices[i];
-        choice.src = choices[i] + '.png';
-        choice.addEventListener('click', selctorChoice);
+        choice.src = choices[i] + ".png";
+        choice.addEventListener("click", selectChoice);
         document.getElementById("choices").append(choice);
     }
 }
 
-function selctorChoice() {
+function selectChoice() {
     you = this.id;
-    document.getElementById("your-choice").src = you + '.png';
-}
-opponent = choices[Math.floor(Math.random() * 3)];
-document.getElementById('opponent-choice').src = opponent + '.png';
+    document.getElementById("your-choice").src = you + ".png";
 
-    if( you == opponent) {
+    //random for oppponent
+    opponent = choices[Math.floor(Math.random() * 3)]; //0- .999999 * 3 = 0-2.99999
+    document.getElementById("opponent-choice").src = opponent + ".png";
+
+    //check for winner
+    if (you == opponent) {
         yourScore += 1;
         opponentScore += 1;
     }
     else {
-        if (you ==  'rock') {
-            if(opponent == 'scissors'){
+        if (you == "rock") {
+            if (opponent == "scissors") {
                 yourScore += 1;
             }
-            else if (opponent == 'paper') {
+            else if (opponent == "paper") {
                 opponentScore += 1;
             }
         }
-        else if (you ==  'scissors') {
-            if(opponent == 'paper'){
+        else if (you == "scissors") {
+            if (opponent == "paper") {
                 yourScore += 1;
             }
-            else if (opponent == 'rock') {
+            else if (opponent == "rock") {
                 opponentScore += 1;
             }
         }
-        else if (you ==  'paper') {
-            if(opponent == 'rock'){
+        else if (you == "paper") {
+            if (opponent == "rock") {
                 yourScore += 1;
             }
-            else if (opponent == 'scissors') {
+            else if (opponent == "scissors") {
                 opponentScore += 1;
             }
         }
-        document.getElementById("your-score").innerText = yourScore;
-        document.getElementById("opponent-score").innerText = opponentScore;
     }
-    
+
+    document.getElementById("your-score").innerText = yourScore;
+    document.getElementById("opponent-score").innerText = opponentScore;
+}
